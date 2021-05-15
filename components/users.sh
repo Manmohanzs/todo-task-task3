de2bab5e0752527c01924577c16d506073d63945
Manmohan506 <manmohan@zelarsoft.com>
2021-05-15 13:01:53 +0530

    508

---
+++ components/users.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+source components/common.sh
+OS_PREREQ
+
+Head "Installing Dependency"2
+apt install openjdk-8-jdk -y &>>$LOG && apt install maven -y &>>$LOG
+stat $?
+
+
+Head "Downloading the component"
+cd /root/
+git clone https://github.com/Manmohan506/users.git &>>$LOG && cd users
+#rm -rf /etc/systemd/system/users.service
+
+
+
+Head "Building the Code"
+mvn clean &>>$LOG && mvn clean package &>>$LOG
+stat $?
+Head "Updating Endpoints"
+mv /root/users/systemd.service /etc/systemd/system/users.service
+sed -i -e "s/LOGIN_ENDPOINT/login.zsdevops01.site/" /etc/systemd/system/users.service
+stat $?
+Head "Starting the Service"
+systemctl daemon-reload &>>$LOG && systemctl start users && systemctl enable users &>>$LOG
+systemctl status users
+
